071a5b14d0c6ac3b376f509632514a4c8eee9a38
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2023-02-15 16:23:48 +0530

    Creating Auth Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
+import React, {useState} from 'react';
 const URL = process.env.URL || 'http://10.2.19.151:4000';
 const socket = socketIO.connect(URL);
 function App() {
+
+  const [user, setUser] = useState({});
+
   return (
     <BrowserRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Home socket={socket} />}></Route>
-          <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+          {
+            user === {} ? <Route path="/auth" element={<Login setUser={setUser} />}></Route> :
+            <>
+              <Route path="/" element={<Home socket={socket} />}></Route>
+              <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+            </>
+          }
         </Routes>
       </div>
     </BrowserRouter>
