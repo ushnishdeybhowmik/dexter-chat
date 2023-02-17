45b497baef50ed1a158772f420c49ab4b05e2433
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2023-02-17 22:38:25 +0530

    Login PAge Tweak #3

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = ({setUser}) => {
+
+  const [userLocal, setUserLocal] = useState('');
+  const [pwdLocal, setPwdLocal] = useState('');
+
+  function userAuth(username, pass){
+    //getUser from MongoDB
+    //return user.pwd === pass
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //if(userAuth(userLocal, pwdLocal)){
+    //  setUser(userLocal);
+    //  style statement
+    //}
+    //else {
+    //  style statement
+    //}
+  }
   return (
     <div className="login-wrapper">
         <div className="login-title">
             User Login
         </div>
         <div className="login-formWrapper">
-            <form className="login-form" method="POST" onSubmit="">
-                <input name="username"></input>
+            <form className="login-form" method="POST" onSubmit={e => handleSubmit(e)} id="loginForm">
+                <input name="username" className="login-input" onChange={(e) => setUserLocal(e.targt.value)}></input>
+                <input name="password" type="password" className="login-password" onChange={(e) => setPwdLocal(e.target.value)}></input>
             </form>
+              <button type="submit" form="loginForm" class="login-button" value="Submit">Submit</button>
         </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
