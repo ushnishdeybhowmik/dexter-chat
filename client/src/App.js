4db2be9d291fa11f541975208e0fbe78fab5e9dc
ushnishdeybhowmik <neilbhowmik05@gmail.com>
2023-02-21 01:17:12 +0530

    Login 2.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 
+
+const navigate = useNavigate();
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+
+    const [user, setUser] = useState({name: '', id: '', socket: undefined});
+    const [socket, setSocket] = useState();
+    useEffect(() => {
+        navigate(`/user/${user.name}`)
+    }, [user]);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                {
+                    user.name ? 
+                    <Route path='/login' element={<Login setUser={setUser} setSocket={setSocket}/>}></Route> :
+                    <Route path={`/user/${user.name}`} element={<Home user={user} socket={socket}/>}></Route>
+                }
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
